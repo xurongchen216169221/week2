5e3c4ed09e59cc4cbba0252961f52b9750217eee
xurongchen216169221 <53501045+xurongchen216169221@users.noreply.github.com>
2019-07-31 23:22:56 +1000

    Add files via upload

---
+++ Activity1.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Drawing;
+using System.Collections.Generic;
+using System;
+using Android.Content;
+using System.Drawing;
+
+namespace _2
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity2);
+
+            ImageView img = FindViewById<ImageView>(Resource.Id.imageView1);
+            var imageSwitch = false;
+            Button button = FindViewById<Button>(Resource.Id.next);
+            Button buttonout = FindViewById<Button>(Resource.Id.@out);
+            button.Click += delegate
+            {
+                if (imageSwitch)
+                {
+                    img.SetImageResource(Resource.Drawable.a);
+                    imageSwitch = false;
+                }
+                else
+                {
+                    img.SetImageResource(Resource.Drawable.back);
+                    imageSwitch = true;
+                }
+            };
+
+            buttonout.Click += (sender, e) =>
+            {
+                Intent intent2 = new Intent(this, typeof(MainActivity));
+                StartActivity(intent2);
+            };
+
+        }
+
+        
+
+        // Create your application here
+
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Drawing;
+using System.Collections.Generic;
+using System;
+using Android.Content;
+
+namespace _2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Button buttonlogin = FindViewById<Button>(Resource.Id.useLogo);
+            Button buttonvistior = FindViewById<Button>(Resource.Id.buttonVisitor);
+            EditText editUserName = FindViewById<EditText>(Resource.Id.useLogo);
+            EditText editPassword = FindViewById<EditText>(Resource.Id.password);
+            buttonvistior.Click += (sender, e) =>
+            {
+                Intent intent1 = new Intent(this, typeof(Activity1));
+               StartActivity(intent1);
+            };
+
+            bool pass = false;
+            bool pass2 = false;
+
+            string name = "313";
+            string password = "123";
+
+
+
+
+            
+                Toast.MakeText(this, "login function are not avaliable now plaese try again later!", ToastLength.Long).Show();
+            
+            
+
+            
+
+                  
+            
+
+        }
+
+
+
+
+
+
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
